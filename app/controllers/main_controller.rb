8f594180fc83f8d0aeb4c6781a444c69a6a0e342
Ken Hom <khom@teamdrg.com>
2018-01-04 20:06:50 -0500

    add main view, controller, and routes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,25 @@
 class MainController < ApplicationController
+
   def index
   end
+
+  def fizz_buzz
+    @num = params[:number]
+
+    @calculator = Calculator.new
+    @result = @calculator.calculate_fizz_buzz(@num.to_i)
+
+    respond_to do |format|
+      format.html
+      format.json {
+        begin
+          Integer(@num)
+          render json: @result.to_json
+        rescue
+          render json: {error: "Sorry, that is not an integer"}, status: 422
+        end
+      }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'main/index'
 
+  get 'main/fizz_buzz' => 'main#fizz_buzz'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
