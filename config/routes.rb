9b2e332cc9ed46f0b46e5d1b0c4fbc86fcf50a0b
Ken Hom <khom@teamdrg.com>
2018-01-04 20:18:17 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'main/fizz_buzz' => 'main#fizz_buzz'
 
+  root 'main#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
